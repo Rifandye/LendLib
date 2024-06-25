a269f57829f151c6e2629f914cba7e24014803ac
Rifandye <rilfransyah@gmail.com>
2024-06-25 17:12:02 +0700

    feat: adding a validation into borrow to prevent user borrowing the same book more than once

--- server/models/borrow.go
+++ server/models/borrow.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"errors"
 	"log"
 	"time"
 
@@ -16,7 +17,29 @@ type Borrow struct {
 }
 
 func (b *Borrow) CreateBorrow(bookId, userId int64) error {
-	query := `
+	//Validating user vould not borrowing the same book more than once
+	var count int
+
+	checkQuery := `
+		SELECT COUNT(*) FROM "Borrows"
+		WHERE "user_id" = $1 AND "book_id" = $2
+	`
+
+	err := db.DB.QueryRow(checkQuery, userId, bookId).Scan(&count)
+
+    if err != nil {
+        log.Printf("Error checking existing borrow: %v", err)
+        return err
+    }
+
+    if count > 0 {
+        return errors.New("user already borrowed this book")
+    }
+
+
+
+	//Inserting borrow data into database
+	insertQuery := `
 		INSERT INTO "Borrows" ("borrowDate", "returnDate", "user_id", "book_id")
 		VALUES ($1, $2, $3, $4)
 		RETURNING id
@@ -29,7 +52,7 @@ func (b *Borrow) CreateBorrow(bookId, userId int64) error {
 
 	var borrowId int64
 
-	err := db.DB.QueryRow(query, borrowDate, returnDate, userId, bookId).Scan(&borrowId)
+	err = db.DB.QueryRow(insertQuery, borrowDate, returnDate, userId, bookId).Scan(&borrowId)
 	if err != nil {
 		log.Printf("Error creating borrow: %v", err)
 		return err
@@ -38,6 +61,7 @@ func (b *Borrow) CreateBorrow(bookId, userId int64) error {
 	b.ID = borrowId
 
 
+	//Updating stock of the book after successfully inserting borrow data
 	updateQuery := `
 		UPDATE "Books"
 		SET "stock" = "stock" - 1
